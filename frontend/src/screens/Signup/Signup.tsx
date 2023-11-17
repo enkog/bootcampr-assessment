1b76a2841e23b4808650538d2386dae38ef60eee
enkog <nkirukaogu@gmail.com>
2023-11-17 09:38:49 +0100

    Add route and layout for signup page

---
+++ frontend/src/screens/Signup/Signup.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Signup.scss'
+
+const Signup = () => {
+  return (
+    <div className='signup__section'>
+      <div className='header__text'>
+        <div className='lead__text'>header text1</div>
+        <div className='sub__text'>header text2</div>
+      </div>
+      <div className='main__signup__section'>
+        <div className='signup__image'>Image</div>
+        <div className='signup__form'>Form</div>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
